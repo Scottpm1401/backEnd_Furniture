b779c5c5a485314cb79530cbc552ef7dc181a229
Nhật Phan <scottpm1401@gmail.com>
2022-11-30 02:07:06 +0700

    add server log and config header

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,88 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import testRouter from './routes/user/index';
-import { db } from './database';
-import { config } from 'dotenv';
-
-config();
+import express from "express";
+import http from "http";
+import mongoose from "mongoose";
+import Logging from "./library/Logging";
+import router from "./routes/user";
+import { config } from "dotenv";
+import cors from "cors";
 
 const app = express();
+config();
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('trust proxy', 1);
-db();
-app.use('/users', testRouter);
+/** Connect to Mongo */
+mongoose
+  .connect(process.env.MONGO_DB || "", { retryWrites: true, w: "majority" })
+  .then(() => {
+    Logging.info("Mongo connected successfully.");
+    StartServer();
+  })
+  .catch((error) => Logging.error(error));
 
-app.listen(process.env.BE_PORT, () => {
-  console.log(`server started on port ${process.env.BE_PORT}`);
-});
+/** Only Start Server if Mongoose Connects */
+const StartServer = () => {
+  /** Log the request */
+  app.use((req, res, next) => {
+    /** Log the req */
+    Logging.info(
+      `Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+    );
+
+    res.on("finish", () => {
+      /** Log the res */
+      Logging.info(
+        `Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`
+      );
+    });
+
+    next();
+  });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors());
+  app.set("trust proxy", 1);
+
+  /** Rules of our API */
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method == "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
+    }
+
+    next();
+  });
+
+  /** Routes */
+  app.use("/user", router);
+
+  /** Healthcheck */
+  app.get("/ping", (req, res, next) =>
+    res.status(200).json({ hello: "world" })
+  );
+
+  /** Error handling */
+  app.use((req, res, next) => {
+    const error = new Error("Not found");
+
+    Logging.error(error);
+
+    res.status(404).json({
+      message: error.message,
+    });
+  });
+
+  http
+    .createServer(app)
+    .listen(process.env.BE_PORT, () =>
+      Logging.info(`Server is running on port ${process.env.BE_PORT}`)
+    );
+};
