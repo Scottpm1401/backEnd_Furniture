fdeba070aa8cd3879bcd70215515f0dab1b7314d
NhatPhan <nhat.phan@executionlab.asia>
2023-05-08 10:31:31 +0700

    refactor analysis by month and year

--- src/middleware/validationSchemas/ParamsSchema.ts
+++ src/middleware/validationSchemas/ParamsSchema.ts
@@ -4,9 +4,6 @@ const ParamsSchema = {
   common: Joi.object({
     id: Joi.string().required(),
   }),
-  month: Joi.object({
-    month: Joi.number().integer().min(1).max(12).required(),
-  }),
 };
 
 export default ParamsSchema;
